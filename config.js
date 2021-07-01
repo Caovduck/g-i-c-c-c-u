b131bb70cb6eec17792bf4354f1f1c33abad24c4
Caovduck <86782796+Caovduck@users.noreply.github.com>
2021-07-01 20:56:14 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
+    introTitle: 'Cậu à!',
     introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
+    Mà sao cậu cứ đi nhầm đường
+    Lạc vào tim tớ lẻ loi
     Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    btnIntro: 'ahihi:))',
+    title: ' Cậu có iu mình khum ?',
+    desc: ':)hihi xàm  ',
+    btnYes: 'yêu cậu vcl <33',
+    btnNo: 'Không,cậu ak.dell và dell :) :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao cậu lại iu thương mình <3',
+    btnReply: 'Gửi cho cậu <3',
+    reply: 'Yêu thì yêu , không yêu thì yêu :))))))))))))',
+    mess: 'hihi tớ biết mà t đẹp zai zl. Yêu cậu nhiều  😘😘',
+    messDesc: 'yêu mình thì ph thương mình nhá ,khum đc bắt nạt mk nhá. yêu <3',
+    btnAccept: 'oki lunnnnnnnnnnn',
+    messLink: 'https://www.facebook.com/profile.php?id=100029762581708' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
